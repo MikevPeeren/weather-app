2ca44b8726bbcd55a5ad6718807fc791e9ced1f2
MikevPeeren <MikevPeeren@hotmail.com>
2020-03-01 16:29:07 +0100

    Added Api Call

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,21 +1,51 @@
 // React
-import React from 'react';
+import React, { RefObject, createRef } from 'react';
 
 // CSS
 import './SearchForm.scss';
 
 // Constants
-import { SEARCH } from '../constants/searchform';
+import { SEARCH, API_URL_WEATHER, API_KEY } from '../constants/searchform';
+
+// Api
+import axios from 'axios';
 
 const SearchForm = () => {
+  let textInput = createRef<HTMLInputElement>();
+
+  const searchCity = () => {
+    if (!textInput.current !== null) {
+      console.log(textInput.current);
+    }
+
+    axios
+      .get(API_URL_WEATHER, {
+        params: {
+          q: 'Tilburg',
+          appid: API_KEY,
+        },
+      })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="SearchForm">
       <input
         className="SearchForm__input"
+        ref={textInput}
         type="text"
         placeholder="Search for a city..."
       />
-      <button className="SearchForm__button" type="submit">
+      <button
+        className="SearchForm__button"
+        type="submit"
+        onClick={() => searchCity()}
+      >
         {SEARCH}
       </button>
     </div>
--- src/constants/searchform.ts
+++ src/constants/searchform.ts
@@ -1,1 +1,5 @@
 export const SEARCH = 'SEARCH';
+
+export const API_URL_WEATHER = 'api.openweathermap.org/data/2.5/weather';
+// Please do not steal :(
+export const API_KEY = '3551881c2b178c722c3ee7657e78f676';
